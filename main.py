7b40731786a5c8148e97080cb35d296a1fdefb1a
Angli Liu <anglil@uw.edu>
2016-04-29 15:25:25 -0700

    not remove question mark

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ if __name__ == "__main__":
     ############################
     # test on single sentence
     ############################
-    question = "how many shelves were filled with carvings"
+    question = "how many potatoes does Mary now have ?"
     answer = "0.5"
     tree = get_parse_tree(question)
     tree.pretty_print()
     sent = qa2hypo(question, answer, True, False)
 
     ############################
-    # test on single sentence
+    # test on all the sentence
     ############################
     # a = get_args()
     # qa_pairs_list = pre_proc(a, 'math')
