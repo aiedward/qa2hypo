f5e3a43254642482d8ce0c2eb713722676266407
Angli Liu <anglil@uw.edu>
2016-05-10 22:39:26 -0700

    runnable

--- main.py
+++ main.py
@@ -14,16 +14,16 @@ if __name__ == "__main__":
     ############################
     # test on single sentence
     ############################
-    # question = "How many pairs of shoes do you have?"
-    # answer = "0.5"
-    # tree = get_parse_tree(question)
-    # tree.pretty_print()
-    # sent = qa2hypo(question, answer, True, False)
+    question = "What is the cactus covered with in the diagram?"
+    answer = "0.5"
+    tree = get_parse_tree(question)
+    tree.pretty_print()
+    sent = qa2hypo(question, answer, True, False)
 
     ############################
     # test on all the sentence
     ############################
-    a = get_args()
-    qa_pairs_list, list_ctr, list_head = pre_proc(a, 'math_aida') # include writing to file when "domain=mat_aida"
-    res = qa2hypo_test(qa_pairs_list)
-    post_proc(a, res, 'math_aida', list_ctr, list_head) # includes writing to file
+    # a = get_args()
+    # qa_pairs_list, list_ctr, list_head = pre_proc(a, 'math_aida') # include writing to file when "domain=mat_aida"
+    # res = qa2hypo_test(qa_pairs_list)
+    # post_proc(a, res, 'math_aida', list_ctr, list_head) # includes writing to file
